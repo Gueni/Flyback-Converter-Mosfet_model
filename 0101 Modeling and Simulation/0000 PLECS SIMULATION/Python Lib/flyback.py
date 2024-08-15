b4472d4ed0220853abe3529c94638df816342188
Mohamed Gueni <guenimystro@gmail.com>
2024-08-15 12:10:38 +0100

    flyback model

---
+++ 0101 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/flyback.py
@@ -0,0 +1,32 @@
+
+#?----------------------------------------------------------------------------------------------------------------------------------------
+#?                                                             ____  ____  ______
+#?                                                            / __ \/ __ )/ ____/
+#?                                                           / / / / __  / /
+#?                                                          / /_/ / /_/ / /___
+#?                                                          \____/_____/\____/
+#?
+#?----------------------------------------------------------------------------------------------------------------------------------------
+import plecs as plc
+import Model_Parameters as mdl
+import post_process
+import cleardata
+#?----------------------------------------------------------------------------------------------------------------------------------------
+port                = "1080"                                                               
+url                 = f"http://localhost:{port}/RPC2"                                      
+mdlvar              = mdl.ModelVars                                                        
+modelname           = "OBC"                                                                
+#?----------------------------------------------------------------------------------------------------------------------------------------
+plcsim              = plc.simpy(url=url , port=port , path=mdl.model_directory , modelvar=mdlvar)    
+plcsim.rpc_connect()                                                                       
+plcsim.load_model()  
+cleardata.clear_data_folders()                                                                  
+#?----------------------------------------------------------------------------------------------------------------------------------------
+plcsim.logParams(str(mdlvar['ToFile']['logfile']),mdlvar)
+plcsim.ClearAllTraces(mdl.scopes)  
+plcsim.Set_sim_param()
+plcsim.launch_sim(modelname=modelname)
+# plcsim.HoldAllTraces(mdl.scopes)
+# plcsim.saveAllTraces(mdl.scopes,mdl,mdlvar['ToFile']['Traces'])
+post_process.gen_plots(resFile= mdlvar['ToFile']['ToFile_path'], html_file=mdlvar['ToFile']['output_html'],OPEN=True)
+#?----------------------------------------------------------------------------------------------------------------------------------------
