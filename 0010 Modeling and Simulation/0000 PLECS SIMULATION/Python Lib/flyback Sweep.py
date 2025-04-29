e1068840545a0614f3777cc1dc9a3e8f257a6b0d
Mohamed Gueni | محمد قاني <20975550+Gueni@users.noreply.github.com>
2025-04-29 14:11:00 +0100

    lhs

--- 0010 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/flyback Sweep.py
+++ 0010 Modeling and Simulation/0000 PLECS SIMULATION/Python Lib/flyback Sweep.py
@@ -15,13 +15,35 @@ import post_process
 import cleardata
 import time
 import os
+from pyDOE import lhs
 #?----------------------------------------------------------------------------------------------------------------------------------------
-port                                          = "1080"                                                               
+port                                          = "7777"                                                               
 url                                           = f"http://localhost:{port}/RPC2"                                      
 modelname                                     = "flyback" 
 mdlvar                                        = mdl.ModelVars
-Vset                                          = (np.arange(5    ,24    +2    ,1    )).tolist()
-Fs                                            = np.linspace(20e3, 250e3, num=len(Vset)).tolist()
+np.random.seed(5)  # For reproducibility
+
+
+# ---- Define parameter bounds ----
+param_bounds                                  = {
+                                                'Vset'  : (1, 24),  
+                                                'Fs'    : (20e3, 250e3)  
+                                                 }   
+param_names                                   = list(param_bounds.keys())
+n_params                                      = len(param_bounds)
+n_samples                                     = 24  # Number of LHS samples
+
+# ---- Generate LHS samples ----
+lhs_raw                                       = lhs(n_params, samples=n_samples, criterion='cm', iterations=10)
+lhs_scaled                                    = []
+
+for sample in lhs_raw:
+    scaled = {
+        name: val * (param_bounds[name][1] - param_bounds[name][0]) + param_bounds[name][0]
+        for val, name in zip(sample, param_names)
+    }
+    lhs_scaled.append(scaled)
+
 plcsim                                        = plc.simpy(url=url , port=port , path=mdl.model_directory , modelvar=mdlvar)   
 #?----------------------------------------------------------------------------------------------------------------------------------------
 plcsim.rpc_connect()                                                                    
@@ -29,8 +51,12 @@ plcsim.load_model()
 plcsim.ClearAllTraces(mdl.scopes)
 cleardata.clear_data_folders()                                                                  
 inc  = 0
-for i, item1 in enumerate(Fs):
-    for j, item2 in enumerate(Vset):
+# ---- STEP 4: Loop through samples ----
+for i, sample in enumerate(lhs_scaled):
+    print(f"Running simulation {i+1}/{n_samples} with: {sample}")
+    
+    # Set parameters in PLECS
+    for param, value in sample.items():
 
         utc_numeric                            = str(int(time.strftime("%Y%m%d%H%M%S",  time.gmtime() )))
         sim_idx                                = inc+1
@@ -40,16 +66,15 @@ for i, item1 in enumerate(Fs):
         mdlvar['ToFile']['logfile']            = str((os.path.join(mdl.current_directory,mdl.logfile_path+f"Log_{utc_numeric}_{sim_idx}.log")).replace("\\", "/"))
         mdlvar['ToFile']['output_html']        = str((os.path.join(mdl.current_directory,mdl.output_html_path+f"Html_{utc_numeric}_{sim_idx}.html")).replace("\\", "/"))
         mdlvar['ToFile']['Traces']             = str((os.path.join(mdl.current_directory,mdl.Traces_path)).replace("\\", "/"))
-        mdlvar['CTRL']['Vset']                 = Vset[j]
-        mdlvar['CTRL']['Fs']                   = Fs[i]
+        
+        mdlvar['CTRL']['param']                 = float(value)
 
         plcsim.logParams(mdlvar['ToFile']['logfile'],mdlvar)
         plcsim.Set_sim_param(mdlvar)
-        plcsim.launch_sim(modelname=modelname)
-        # plcsim.HoldAllTraces(mdl.scopes)
-        # plcsim.saveAllTraces(mdlvar['scopes'],mdl,mdlvar['ToFile']['Traces'])
-        post_process.gen_plots(resFile= mdlvar['ToFile']['ToFile_path'], html_file=mdlvar['ToFile']['output_html'],OPEN=False)
+        # plcsim.launch_sim(modelname=modelname)
+        # # plcsim.HoldAllTraces(mdl.scopes)
+        # # plcsim.saveAllTraces(mdlvar['scopes'],mdl,mdlvar['ToFile']['Traces'])
+        # post_process.gen_plots(resFile= mdlvar['ToFile']['ToFile_path'], html_file=mdlvar['ToFile']['output_html'],OPEN=False)
         inc+=1
 #?----------------------------------------------------------------------------------------------------------------------------------------
-
 
